93dca976b7c8bc1bf1623663f197ce8f36d9b41e
劉奕德 <liouyide@liuyidedeMacBook-Air.local>
2024-03-28 19:11:16 +0800

    feat: adjust list layout

--- app/page.js
+++ app/page.js
@@ -1,4 +1,16 @@
-export default function HomePage() {
+async function getData() {
+  const res = await fetch('http://localhost:3000/api/repos');
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
+export default async function HomePage() {
+  const data = await getData();
+
   return (
     <>
       <div className="flex flex-row w-full items-center p-8 ">
@@ -15,7 +27,22 @@ export default function HomePage() {
           Search
         </label>
       </div>
-      <ul role="list" className="divide-y divide-gray-100"></ul>
+      <ul role="list" className="divide-y divide-gray-700 ">
+        {data.response.items.map((repo) => (
+          <li key={repo.id} className="p-8">
+            <div className="flex min-w-0 gap-x-4">
+              <div className="min-w-0 flex-auto">
+                <p className="text-sm font-semibold leading-6 text-gray-900">
+                  {repo.full_name}
+                </p>
+                <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                  {repo.description}
+                </p>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
